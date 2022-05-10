5844fd62e15c25dd272272d5d773cc7b67c32d04
techshinevietnam <cn20082000@gmail.com>
2022-05-10 23:31:33 +0700

    shipment entity

---
+++ acom-shipment/src/main/kotlin/com/acom/acomshipment/api/ShipmentApiController.kt
@@ -0,0 +1,24 @@
+package com.acom.acomshipment.api
+
+import com.acom.acomshipment.entity.Shipment
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@CrossOrigin
+@RequestMapping
+class ShipmentApiController {
+
+    val shipments = listOf(
+        Shipment(1, "Giao Hàng Tiết Kiệm", 100000.0),
+        Shipment(2, "Giao hàng nhanh", 120000.0),
+        Shipment(3, "VNPost", 150000.0),
+        Shipment(4, "ViettelPost", 70000.0),
+        Shipment(5, "Kerry TTC Express", 200000.0),
+        Shipment(6, "DHL Expres", 110000.0),
+    )
+
+    @GetMapping(params = ["id"])
+    fun getShipment(@RequestParam id: Long): Shipment? {
+        return shipments.firstOrNull { s -> s.id == id }
+    }
+}
---
+++ acom-shipment/src/test/kotlin/com/acom/acomshipment/AcomShipmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.acom.acomshipment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AcomShipmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
