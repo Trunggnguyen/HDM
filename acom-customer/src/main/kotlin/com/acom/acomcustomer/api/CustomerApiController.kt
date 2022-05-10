6e5cc99251200df3b4fdb59a03d4c410a8c25b10
techshinevietnam <cn20082000@gmail.com>
2022-05-10 23:47:14 +0700

    customer tel

--- acom-customer/src/main/kotlin/com/acom/acomcustomer/api/CustomerApiController.kt
+++ acom-customer/src/main/kotlin/com/acom/acomcustomer/api/CustomerApiController.kt
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.*
 class CustomerApiController {
 
     val customers = listOf(
-        Customer(1L, "Nguyễn Công Chính", "Hà Nội", "cn20082000@gmail.com", "1988889962"),
-        Customer(2L, "Phạm Đức Minh", "Hà Nội", "cn20082000@gmail.com", "1988889962"),
-        Customer(3L, "Nguyễn Văn Trung", "Hà Nội", "cn20082000@gmail.com", "1988889962"),
+        Customer(1L, "Nguyễn Công Chính", "Hà Nội", "0123456789", "cn20082000@gmail.com", "1988889962"),
+        Customer(2L, "Phạm Đức Minh", "Hà Nội", "0936724836",  "cn20082000@gmail.com", "1988889962"),
+        Customer(3L, "Nguyễn Văn Trung", "Hà Nội", "078716237", "cn20082000@gmail.com", "1988889962"),
     )
 
     @GetMapping(params = ["id"])
--- acom-customer/src/main/kotlin/com/acom/acomcustomer/entity/Customer.kt
+++ acom-customer/src/main/kotlin/com/acom/acomcustomer/entity/Customer.kt
@@ -4,6 +4,7 @@ data class Customer(
     val id: Long? = null,
     val name: String? = null,
     val address: String? = null,
+    val tel: String? = null,
     val email: String? = null,
     val telegramId: String? = null,
 )
